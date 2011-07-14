8e54b2f61da58c81fb9f273227cabc06eb5cdd69
Denis Zontak <dzontak@gmail.com>
2011-07-14 07:16:22 -0400

    renamed to insta4j

---
+++ insta4j-core/src/main/java/org/idm/jinstapaper/client/InvalidCredentialsException.java
@@ -0,0 +1,26 @@
+package org.idm.jinstapaper.client;
+
+
+/**
+ * A runtime exception thrown by a InstapaperClient that signals a
+ * failure to authenticate a user.
+ *
+ * @author dzontak@gmail.com
+ */
+public class InvalidCredentialsException extends RuntimeException {
+	public InvalidCredentialsException() {
+		super();
+	}
+
+	public InvalidCredentialsException(final String message) {
+		super(message);
+	}
+
+	public InvalidCredentialsException(final Throwable cause) {
+		super(cause);
+	}
+
+	public InvalidCredentialsException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+}
