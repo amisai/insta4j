7a649a4b67bce2408add490b689d5ff93be4e079
Denis Zontak <dzontak@gmail.com>
2011-07-23 12:15:15 -0400

    added Copyright headers

--- insta4j-client/src/main/java/org/ooloo/insta4j/jaxb/InstaRecordBean.java
+++ insta4j-client/src/main/java/org/ooloo/insta4j/jaxb/InstaRecordBean.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package org.ooloo.insta4j.jaxb;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -5,8 +19,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * A jaxb annotated bean which can bind any json record retured by Full Instapaper Api.
+ * @href http://www.instapaper.com/api/full
  *
  * @author dzontak@gmail.com
+ *
  */
 @XmlRootElement(name = "instarecord")
 public class InstaRecordBean {
